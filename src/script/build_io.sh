b0717bbbec3e4bcff92cf84e59041bee101dc0b4
Renzo Dani <arons7@gmail.com>
2024-06-14 23:50:29 +0200

    build github

--- src/script/build_io.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf ../../target/classes
-mkdir -p ../../target/classes
-mkdir -p ../../target/bin
-
-javac -sourcepath ../main/ ../main/ch/arons/jbench/Main.java -d ../../target/classes
-
-native-image  -cp ../../target/classes/ ch.arons.jbench.Main -o ../../target/bin/jbench

3fdb9165ca70fe3a822be7a84395fd3770afdf6d
Renzo Dani <arons7@gmail.com>
2024-06-14 23:48:37 +0200

    test again build on github

---
+++ src/script/build_github.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export JAVA_HOME=/home/arons/prog/java/gjdk-21/
+
+rm -rf ../../target/classes
+mkdir -p ../../target/classes
+mkdir -p ../../target/bin
+
+$JAVA_HOME/bin/javac -sourcepath ../main/ ../main/ch/arons/jbench/Main.java -d ../../target/classes
+
+$JAVA_HOME/bin/native-image  -cp ../../target/classes/ ch.arons.jbench.Main -o ../../target/bin/jbench

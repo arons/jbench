c19d4abdb32e1c86316267dae52a1a18b79b8de7
Renzo Dani <arons7@gmail.com>
2024-06-15 21:56:35 +0200

    test with avg and max

--- src/main/ch/arons/jbench/pg/test/OpenLatency.java
+++ src/main/ch/arons/jbench/pg/test/OpenLatency.java
@@ -16,15 +16,42 @@ public class OpenLatency extends DBTest {
     
     @Override
     public void test() {
+        int numberTest = 10;
+        
+        int count = 0;
+        long totConnMs = 0;
+        long maxConnMs = 0;
+        long minConnMs = Long.MAX_VALUE;
+        for (int i = 0; i < numberTest; i++) {
+            long currentMs = testConnectio();
+            if (currentMs > 0) {
+                count++;
+                totConnMs += currentMs;
+                if (maxConnMs < currentMs) {
+                    maxConnMs = currentMs;
+                }
+                if (minConnMs > currentMs) {
+                    minConnMs = currentMs;
+                }
+            }
+        }
+        
+        System.out.printf("Connection open latency:\n");
+        System.out.printf(" avg: %d ms \n", totConnMs / (count));
+        System.out.printf(" max: %d ms \n", maxConnMs);
+        System.out.printf(" min: %d ms \n", minConnMs);
+    }
+
+    private long testConnectio() {
         Connection c = null;
         try {
             long start = System.currentTimeMillis();
             c = getConnection();
             long duration = System.currentTimeMillis() - start;
-            System.out.printf("Connection open latency: %d ms \n", duration);
+            return duration;
         } catch (SQLException e) {
             System.err.println("could not connect to DB: " + e.getMessage());
-            return;
+            return -1;
         } finally {
             if (c != null) {
                 try { 

f832a698c89284ff86e1111da416f79342329aeb
Renzo Dani <arons7@gmail.com>
2024-07-01 21:27:47 +0200

    version 1.1

--- src/main/ch/arons/jbench/Config.java
+++ src/main/ch/arons/jbench/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     private Config() {}
     
-    public static final String VERSION = " 1.0";
+    public static final String VERSION = " 1.1";
     public static final int KILOBYTE = 1024;
     public static final int MEGABYTE = 1024 * KILOBYTE;
 
